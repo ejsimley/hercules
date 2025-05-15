8b3e23af7a08627ee57e1a3df902aedc5f0359f3
Eric Simley <eric.simley@nrel.gov>
2025-05-15 15:44:06 -0600

    adding resource_downloading folder

---
+++ hercules/tools/resource_downloading/upsample_wind_data.py
@@ -0,0 +1,5 @@
+"""
+This module contains functions for generating wind time series at turbine locations by
+a) spatially interpolating wind data at grid locations from the WTK-LED dataset and 
+b) temporally upsampling the time series and adding turbulence.
+"""
